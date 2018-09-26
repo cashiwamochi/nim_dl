3b640a1993e38706568e6d62884a1974d3dbcdf4
Ryotaro Yada <ryo@RyoMBP.local>
2018-09-27 01:40:53 +0900

    initial commit

---
+++ cifar10.nim
@@ -0,0 +1,101 @@
+import os
+import streams, sequtils, random
+import arraymancer
+
+
+const 
+  CIFAR10Filenames = [
+    "data_batch_1.bin",
+    "data_batch_2.bin",
+    "data_batch_3.bin",
+    "data_batch_4.bin",
+    "data_batch_5.bin",
+    "test_batch.bin"
+    ]
+
+  height = 32
+  width = 32
+  c = 3
+  num_patches_in_file = 10000
+  num_data_in_row = 3073
+
+type 
+  cifar10 = tuple[
+    train_images: Tensor[uint8],
+    test_images: Tensor[uint8],
+    train_labels: Tensor[uint8],
+    test_labels: Tensor[uint8]
+    ]
+
+  cifar10Temp = tuple[
+    images: Tensor[uint8],
+    labels: Tensor[uint8]
+    ]
+
+  patchImageData = array[0 .. c-1, array[0 .. height-1, array[0 .. width-1, uint8]]]
+
+proc checkCifarExist(cifar_dir: string, cifar_file_names: array[0..5, string]) : bool = 
+  for file_name in cifar_file_names:
+    if not existsFile(cifar_dir & "/" & file_name):
+      return false
+  return true
+
+proc read_cifar10*(cifar10_dir: string, file_names: seq[string]): cifar10Temp = 
+  var
+    cifar10_image_patches = newSeq[patchImageData](10000*len(file_names)) # batch * c * h * w
+    cifar10_labels = newSeq[uint8](10000*len(file_names)) # batch * 1
+
+  for file_idx, file_name in file_names:
+    var fs = newFileStream(cifar10_dir & "/" & file_name, fmRead)
+    if not isNil(fs):
+      var 
+        col_idx = 0 # 0 - 3072
+        row_idx = 0 # 0 - 9999
+      let
+        offset = num_data_in_row*file_idx
+      while not fs.atEnd:
+        let value : uint8 = fs.readUint8()
+        if col_idx == 0: # 0
+          cifar10_labels[row_idx + offset] = value
+        elif col_idx <= 1024: # 1 - 1024 r
+          cifar10_image_patches[row_idx + offset][0][(col_idx-1) div width][(col_idx-1) mod width] = value
+        elif col_idx <= 2048: # 1025 - 2048 g
+          cifar10_image_patches[row_idx + offset][1][(col_idx-1025) div width][(col_idx-1025) mod width] = value
+        elif col_idx <= 3072: # 2049 - 3072 b
+          cifar10_image_patches[row_idx + offset][2][(col_idx-2049) div width][(col_idx-2049) mod width] = value
+        else:
+          echo "|| [ERROR] Ploblem in parsing"
+          break
+  
+        col_idx = col_idx + 1
+  
+        if col_idx == num_data_in_row:
+          col_idx = 0
+          row_idx = row_idx + 1
+      
+      assert row_idx == num_patches_in_file
+    fs.close()
+  
+  # var r = Rand()
+  # shuffle(r,cifar10_image_patches)
+  # shuffle(r,cifar10_labels)
+
+  result.images = cifar10_image_patches[0 .. 20000].toTensor()
+  result.labels = cifar10_labels[0 .. 20000].toTensor()
+
+proc load_cifar10*(cifar10_dir: string): cifar10 =
+  if not checkCifarExist(cifar10_dir, CIFAR10Filenames):
+    echo "Not found cifar10 ..."
+    quit(1)
+
+  # train
+  let train_cifar: cifar10Temp = read_cifar10(cifar10_dir, CIFAR10Filenames[0..4])
+
+  # test
+  let test_cifar: cifar10Temp = read_cifar10(cifar10_dir, @[CIFAR10Filenames[5]])
+
+  result.train_images = train_cifar.images
+  result.train_labels = train_cifar.labels
+
+  result.test_images = test_cifar.images
+  result.test_labels = test_cifar.labels
---
+++ demoCifar10.nim
@@ -0,0 +1,83 @@
+import os
+import streams, sequtils
+import math, random
+import arraymancer
+
+import cifar10
+
+# main
+echo "||================================= Cifar-10 (Nim) ================================= "
+let 
+  root_path : string = os.getAppDir()
+  data = loadCifar10(root_path & "/cifar10")
+  x_train = data.train_images.astype(float32) / 255.0'f32
+  y_train = data.train_labels.astype(int)
+  x_test = data.test_images.astype(float32) / 255.0'f32
+  y_test = data.test_labels.astype(int)
+
+echo "|| Arraymancer environment is being arranged ... "
+
+randomize(8) # Random seed for reproducibility
+
+let
+  ctx = newContext Tensor[float32] # Autograd/neural network graph
+  n = 32                           # Batch size
+  c = 3
+  h = 32
+  w = 32
+
+  X_train = ctx.variable x_train
+  X_test = ctx.variable x_test
+
+# Configuration of the neural network
+network ctx, DemoNet:
+  layers:
+    x:          Input([c, h, w])
+    cv1:        Conv2D(x.out_shape, 20, 5, 5)
+    mp1:        MaxPool2D(cv1.out_shape, (2,2), (0,0), (2,2))
+    cv2:        Conv2D(mp1.out_shape, 50, 5, 5)
+    mp2:        MaxPool2D(cv2.out_shape, (2,2), (0,0), (2,2))
+    fl:         Flatten(mp2.out_shape)
+    hidden:     Linear(fl.out_shape, 500)
+    classifier: Linear(500, 10)
+  forward x:
+    x.cv1.relu.mp1.cv2.relu.mp2.fl.hidden.relu.classifier
+
+let 
+  model = ctx.init(DemoNet)
+  optim = model.optimizerSGD(learning_rate = 0.001'f32)
+
+# Learning loop
+echo "|| Learning Start !"
+for epoch in 0 ..< 30:
+  # Back-Propagation-Part
+  for batch_id in 0 ..< data.train_images.shape[0] div n:
+    let 
+      offset = batch_id * n
+      batch_data_x = X_train[offset ..< offset+n]
+      batch_data_y = y_train[offset ..< offset+n]
+      clf = model.forward(batch_data_x)
+      loss = clf.sparse_softmax_cross_entropy(batch_data_y)
+
+    if batch_id mod 100 == 0:
+      echo "|| Epoch is: " & $epoch
+      echo "|| Batch id: " & $batch_id
+      echo "|| Loss is:  " & $loss.value.data[0]
+
+    loss.backprop()
+    optim.update()
+
+  # Validation
+  ctx.no_grad_mode:
+    echo "|| Epoch #" & $epoch & " done. Testing accuracy"
+
+    var score = 0.0
+    var loss = 0.0
+    for i in 0 ..< 100:
+      let y_pred = model.forward(X_test[i*100 ..< (i+1)*100]).value.softmax.argmax(axis = 1).squeeze
+      score += accuracy_score(y_test[i*100 ..< (i+1)*100], y_pred)
+      loss += model.forward(X_test[i*100 ..< (i+1)*100]).sparse_softmax_cross_entropy(y_test[i*100 ..< (i+1)*100]).value.data[0]
+    score /= 100.0
+    loss /= 100.0
+    echo "|| Accuracy: " & $(score * 100.0) & "%"
+    echo "|| Loss:     " & $loss
